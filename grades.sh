41819d2599f3880e2407366fba95797ab744785c
Sherman <gtnovota@gmail.com>
2019-06-03 20:08:56 -0600

    update

--- grades.sh
+++ grades.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+# execute chmod +x grades.sh first
 # scirpt withoug using the UNIX 'awk' command
 
 echo "FileName: " $1
+
+# IFS= (or IFS='') prevents leading/trailing whitespace from being trimmed
+# -r prevents backslash escapes from being interpreted
+
+# read each line
+while IFS= read -r line; do
+	echo "Text read from file: $line"
+	# read each bit of info in the line and store in array
+	array=()
+	for element in $line
+	do
+		echo "$element"
+		array+=("$element")
+		echo ${#array[@]}
+	done
+
+done < "$1"

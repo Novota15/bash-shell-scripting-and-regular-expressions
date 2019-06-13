f1046c54386b79b8ecea3a72246219aa5c9b1fad
Grant Novota <47872713+Novota15@users.noreply.github.com>
2019-06-12 20:27:49 -0600

    Update grades.sh

--- grades.sh
+++ grades.sh
@@ -29,5 +29,6 @@ while read first second third fourth fifth sixth; do
 	let sum=($fourth+$fifth+$sixth)
 	let avg=($sum/3)
 	echo "average: $avg" 
-	echo "$avg $first $third $second" >> sortedStudentsGrades.txt
+	echo " " > sortedStudentsGrades.txt
+	echo "$avg $first $third $second" >> sorted($1).txt
 done < $1

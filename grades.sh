4ed1fab3c652ff6b10b2d5ceb67fe811523ca592
Grant Novota <47872713+Novota15@users.noreply.github.com>
2019-06-12 20:40:15 -0600

    Update grades.sh

--- grades.sh
+++ grades.sh
@@ -32,4 +32,14 @@ while read first second third fourth fifth sixth; do
 	echo "$avg $first $third $second" >> average$1
 done < $1
 
+echo "sorted by average:"
 sort -r -k1 average$1
+
+echo "sorted by last name:"
+sort -k3 average$1
+
+echo "sorted by first name:"
+sort -k4 average$1
+
+echo "sorted by id:"
+sort -k2 average$1

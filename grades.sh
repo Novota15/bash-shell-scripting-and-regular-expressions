d4af51261b54d0f794742ecf41eb12ac848c1c75
Grant Novota <47872713+Novota15@users.noreply.github.com>
2019-06-12 19:11:27 -0600

    Update grades.sh

--- grades.sh
+++ grades.sh
@@ -9,15 +9,20 @@ echo "FileName: " $1
 # -r prevents backslash escapes from being interpreted
 
 # read each line
-while IFS= read -r line; do
-	echo "Text read from file: $line"
-	# read each bit of info in the line and store in array
-	array=()
-	for element in $line
-	do
-		echo "$element"
-		array+=("$element")
-		echo ${#array[@]}
-	done
+# while IFS= read -r line; do
+# 	echo "Text read from file: $line"
+# 	# read each bit of info in the line and store in array
+# 	array=()
+# 	for element in $line
+# 	do
+# 		echo "$element"
+# 		array+=("$element")
+# 		echo ${#array[@]}
 
-done < "$1"
+# 	done
+
+# done < "$1"
+
+while read fourth fifth sixth; do
+	average = (fourth + fifth + sixth) / 3
+	echo "average: $average" 

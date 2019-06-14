9c4e72acaf23120541e0e9278c3ba7cdf62bd645
Grant Novota <47872713+Novota15@users.noreply.github.com>
2019-06-13 20:01:07 -0600

    Update regexAnswers.sh

--- regexAnswers.sh
+++ regexAnswers.sh
@@ -8,15 +8,15 @@
 [[ -n "$1" ]] && echo "FileName=$1" || echo "Usage: regexAnswers.sh filename"
 
 # count lines ending with number or alphabetic letter
-grep '$[A-Za-z|0-9]' $1 | wc -l
+grep '$[A-Za-z]|[0-9]' $1 | wc -l
 # number of lines that do not start with a vowel
 grep '^[AEIOUaeiou]' $1 | wc -l
 # number of lines that have 12 or more alphabetic letters
-grep '[A-Z\{12,\}]' $1 | wc -l
+grep '[A-Za-z\{12,\}]' $1 | wc -l
 # number of lines containing only numbers
 grep '[0-9]' $1 | wc -l
 # number of email addresses from UCDenver (e.g. end with 'UCDenver.edu')
-grep -i 'UCDenver.edu' $1 | wc -l
+grep -i 'UCDenver.edu' $1 | wc -l #confirmed
 # number of phone numbers in the file (i.e. format ___-___-____)
 
 # number of city of Boulder phone numbers (303 numbers)

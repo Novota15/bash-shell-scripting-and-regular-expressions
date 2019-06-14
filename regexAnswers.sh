283e63c6ebdca785588295990308d2fc9d448dca
Grant Novota <47872713+Novota15@users.noreply.github.com>
2019-06-13 20:27:18 -0600

    Update regexAnswers.sh

--- regexAnswers.sh
+++ regexAnswers.sh
@@ -10,7 +10,7 @@
 # count lines ending with number or alphabetic letter
 grep '[A-Za-z|0-9]\+$' $1 | wc -l #good
 # number of lines that do not start with a vowel
-grep '^[^AEIOUaeiou]' $1 | wc - #good
+grep '^[^AEIOUaeiou]' $1 | wc -l #good
 # number of lines that have 12 or more alphabetic letters
 grep '[A-Za-z\{12,\}]' $1 | wc -l
 # number of lines containing only numbers
@@ -18,7 +18,7 @@ grep '[0-9]' $1 | wc -l
 # number of email addresses from UCDenver (e.g. end with 'UCDenver.edu')
 grep -i 'UCDenver.edu' $1 | wc -l #confirmed
 # number of phone numbers in the file (i.e. format ___-___-____)
-grep '\b[[:digit:]]{3}-[[:digit:]]{3}-[[:digit:]]{4}\b' $1 | wc -l
+egrep '\b[[:digit:]]{3}-[[:digit:]]{3}-[[:digit:]]{4}\b' $1 | wc -l
 # number of city of Boulder phone numbers (303 numbers)
 
 # number of lines begining with a vowel and ending with a number

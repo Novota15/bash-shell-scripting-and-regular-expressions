ae49e1db675cff2f8523d80e55208415ec097315
Grant Novota <47872713+Novota15@users.noreply.github.com>
2019-06-15 19:20:17 -0600

    Update gradesAwk.sh

--- gradesAwk.sh
+++ gradesAwk.sh
@@ -2,3 +2,10 @@
 
 # script using the UNIX 'awk' command
 
+[[ -n "$1" ]] && echo "FileName=$1" || echo "Usage: grades.sh filename"
+
+# Using the awk command:
+# 1. awk 'FNR > 1 { sum=0; for (col=3; col<=NF; col++) sum += $col; print $1 " " $2 " " sum/6; }' grades.txt
+# 2. awk 'FNR > 1 {avg; for (col=6; col<=NF; col++) avg += $col/3;} END {print "Average = ", avg/6}' grades.txt, this gives 80.39
+
+awk '{avg; for (col=4; col<=NF; col++) avg += $col/3; print "$avg $col=1 $col=3 $col=2" }' $1
